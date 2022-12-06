00fe08a2e51f7a95d53e97cdc6ef1cb50e06c35d
Neygone <huyghegabriel.sn@gmail.com>
2022-12-06 11:58:23 +0100

    increaseWidth skills bars animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,25 +16,43 @@ export class AppComponent {
 				const modal = document.querySelector('#modalId') as HTMLElement | null;
 				modal!.style.display = 'flex';
 				const skills_bars: any = [];
-				const widths = [90, 80, 70, 50, 50, 50, 50, 35, 35, 10];
-				let width = 0;
+				const widths = [90, 80, 70, 50, 50, 50, 50, 35, 35, 10]; // HTML, CSS, JavaScript, PHP, SQL, TypeScript, Angular, NodeJs, Git, MongoDB
+				// let width = 0;
 
-				// // for(let i = 0; i <= 10; i++) {
-				// // 	let skill_bar = document.querySelector(`#skill-bar${i}`) as HTMLElement | null;
-				// // 	skills_bars.push(skill_bar);
-				// // 	if(width < widths[i]) {
-				// // 		setInterval(() => {
-				// // 			width += 0.05;
-				// // 			skills_bars[i]!.style.width = `${width}%`;
-				// // 		}, 10);
-				// // 	}
-				// }
+				for(let i = 0; i < widths.length; i++) {
+					let skill_bar = document.querySelector(`#skill-bar${i}`) as HTMLElement | null;
+					skills_bars.push(skill_bar);
+					this.increaseWidth(i, widths[i], skills_bars[i]);
+				}
 			}, 20);
 
 			circle!.onmouseout = () => setTimeout(() => {
-				const modal = document.querySelector('#modalId') as HTMLElement | null;
-				modal!.style.display = 'none';
-			}, 2000);
+				const skills_bars: any = [];
+				const widths = [90, 80, 70, 50, 50, 50, 50, 35, 35, 10]; // HTML, CSS, JavaScript, PHP, SQL, TypeScript, Angular, NodeJs, Git, MongoDB
+
+				for(let i = 0; i < widths.length; i++) {
+					let skill_bar = document.querySelector(`#skill-bar${i}`) as HTMLElement | null;
+					skills_bars.push(skill_bar);
+					this.decreaseWidth(i, widths[i], skills_bars[i]);
+				}
+
+				// const modal = document.querySelector('#modalId') as HTMLElement | null;
+				// modal!.style.display = 'none';
+			}, 20);
 		}
+  }
+
+  increaseWidth(i: number = 0, skill_bar_width : number, skill_bar: HTMLElement | null): void {
+	setInterval(() => {
+		i += 0.5;
+		if(i <= skill_bar_width) skill_bar!.style.width = `${i}%`;
+	}, 10);
+  }
+
+  decreaseWidth(i: number = 0, skill_bar_width : number, skill_bar: HTMLElement | null): void {
+	setInterval(() => {
+		i -= 0.5;
+		if(i >= 0) skill_bar!.style.width = `${i}%`;
+	}, 10);
   }
 }
